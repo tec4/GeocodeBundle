0675b631c070eb51d279d435c93deefcf1ac1e71
tec4 <watts.eric@yahoo.com>
2019-05-19 18:30:10 -0700

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tec4_geocode');
+        $treeBuilder = new TreeBuilder('tec4_geocode);
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
